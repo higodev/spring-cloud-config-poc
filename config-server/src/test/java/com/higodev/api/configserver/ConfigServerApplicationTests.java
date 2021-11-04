2227f90baff644cdbdfa89cd7975b8b47d38d3b2
Higo Alexandre <higoalexandreb@gmail.com>
2021-11-04 07:49:54 -0300

    first commit

---
+++ config-server/src/test/java/com/higodev/api/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.higodev.api.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
